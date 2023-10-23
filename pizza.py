9c1c269782200670a62e8d76a4f41b117b96a956
Will Einhorn <einhornwill@gmail.com>
2023-10-22 20:51:17 -0400

    New file

---
+++ pizza.py
@@ -0,0 +1,16 @@
+pizzas = ["pepproni", "cheese", "supreme"]
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I like pizza.")
+
+
+animals = ["tiger", "cat", "lion"]
+for animal in animals: 
+    print(f"A {animal} would make a great  pet.")
+print("These are all cats.")
+
+
+
+    
+
+    
