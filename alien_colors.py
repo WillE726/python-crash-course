0cb1bbf1f5d2fd0d6ad9eb1128d77b2e78ec1bf3
William Einhorn <einhornwill@gmail.com>
2023-11-09 10:39:39 -0500

    new

---
+++ alien_colors.py
@@ -0,0 +1,29 @@
+alien_color = "green"
+if alien_color == "green":
+    print("You have just earned 5 points.")
+
+if alien_color == "yellow":
+    print("No points")
+
+if alien_color == "green":
+    print("You just earned 5 points.")
+else:
+    print("You have earned 10 points")
+
+if alien_color == "yellow":
+    print("You just earned 5 points.")
+else:
+    print("You have earned 10 points")
+
+
+alien_color = "yellow"
+if alien_color == "green":
+    print("You earned 5 points.")
+elif alien_color ==  "yellow":
+    print("You earned 10 points.")
+elif alien_color == "red":
+    print("You earned 15 points")
+
+
+
+
---
+++ favorite_fruit.py
@@ -0,0 +1,6 @@
+fruits = ["banana", "cherry", "apple", "orange", "lime"]
+if "banana" in fruits:
+    print(f"You really like bananas.")
+if "blueberries" in fruits:
+    print("You really like blueberries.")
+    
